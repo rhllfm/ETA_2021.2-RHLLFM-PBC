3dd8b8252229dd2c2723eb0982637aacd12a23e6
rhllfm <rhllfm@cesar.school>
2022-06-19 13:28:55 -0300

    commit test 4

--- tests/test_4.py
+++ tests/test_4.py
@@ -15,6 +15,6 @@ class Test4:
         assert user_page.confirmar_operacao(user_page.valorDeposito), "Deposited value is different from the expected."
         user_page.informar_valor(user_page.valorDeposito)
         assert user_page.is_page(user_page.url_transaction), "Transaction page not found"
-        assert user_page.get_qtd_transactions() >= 1, 'Quantidade de transacões incorreta'
+        assert user_page.get_qtd_transactions() == 1, 'Quantidade de transacões incorreta'
         user_page.reset_transactions()
         assert user_page.get_qtd_transactions() == 0, 'Transações nao foram resetadas'
